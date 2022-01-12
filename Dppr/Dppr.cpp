ffb40a6c18c520266fe4e005c285371e9a42b993
Dplepler <david.plepler@gmail.com>
2022-01-12 18:18:54 +0200

    Window application

--- Dppr/Dppr.cpp
+++ Dppr/Dppr.cpp
@@ -1,17 +1,11 @@
 #include "utils.h"
 
-
-int main(int argc, char** argv) {
-
-	if (argc < 2) {
-		printf("Argument missing. For this program to run you have to pass an 'ok' argument.\n");
-		return 1;
-	}
-
-	if (strcmp(argv[1], "ok")) return 1;
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
 
 	//createNote(".\\note.txt", "HI HELLO THERE", TRUE);
 	while (popup("lol", "Still using this computer?\n") == IDYES) { }
+
+
 
 	return 0;
 }
